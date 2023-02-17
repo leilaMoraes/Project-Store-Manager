e800bd3b465c06b058309fafd98c6386c023c332
Leila Moraes <leila.violino@gmail.com>
2023-02-17 11:50:32 -0300

    feat: cria função getProducts no arquivo productsController.js

---
+++ src/controllers/productsController.js
@@ -0,0 +1,13 @@
+const productsService = require('../services/productsService');
+
+const OK = 200;
+
+const getProducts = async (_req, res) => {
+  const { type, message } = await productsService.getAllProducts();
+
+  if (type) return res.status(type).json(message);
+
+  return res.status(OK).json(message);
+};
+
+module.exports = { getProducts };
