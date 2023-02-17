bacab14f04efaedea9912f9861f81597644b5367
Leila Moraes <leila.violino@gmail.com>
2023-02-17 16:26:06 -0300

    feat: função model para identificar o product pelo id

--- src/models/productsModels.js
+++ src/models/productsModels.js
@@ -7,4 +7,12 @@ const getAllProducts = async () => {
   return result;
 };
 
-module.exports = { getAllProducts };
+const getProductById = async (id) => {
+  const [[result]] = await connection.execute(
+    'SELECT * FROM StoreManager.products WHERE id = ?',
+    [id],
+  );
+  return result;
+};
+
+module.exports = { getAllProducts, getProductById };
