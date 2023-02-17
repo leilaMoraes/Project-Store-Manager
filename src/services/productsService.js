9edeae51d1605c1e9dd3323c90dc9731e56aa7a5
Leila Moraes <leila.violino@gmail.com>
2023-02-17 16:37:27 -0300

    feat: função getProductById no productsService

--- src/services/productsService.js
+++ src/services/productsService.js
@@ -1,8 +1,16 @@
 const { productsModels } = require('../models');
 
+const NOT_FOUND = 404;
+
 const getAllProducts = async () => {
   const allProducts = await productsModels.getAllProducts();
   return { type: null, message: allProducts };
 };
 
-module.exports = { getAllProducts };
+const getProductById = async (id) => {
+  const product = await productsModels.getProductById(id);
+  if (product) return { type: null, message: product };
+  return { type: NOT_FOUND, message: 'Product not found' };
+};
+
+module.exports = { getAllProducts, getProductById };
