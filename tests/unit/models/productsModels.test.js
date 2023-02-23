482a5dce99caa054b163b8170b342411a23a5eb9
Leila Moraes <leila.violino@gmail.com>
2023-02-22 21:42:32 -0300

    test: adiciona teste para a função getProductById no model

--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -4,10 +4,20 @@ const connection = require("../../../src/models/connection");
 const { productsModels } = require("../../../src/models");
 const { allProducts } = require("./mock");
 
-describe("Testa a camada model para a rota /products", function () {
-  it("Retorna todos os produtos cadastrados (model)", async function () {
-    sinon.stub(connection, "execute").resolves([allProducts]);
+describe('Testa a camada model para a rota /products', function () {
+  it('Retorna todos os produtos cadastrados (model)', async function () {
+    sinon.stub(connection, 'execute').resolves([allProducts]);
     const result = await productsModels.getAllProducts();
     expect(result).to.be.deep.equal(allProducts);
+  });
+
+  it('Retorna um produto pelo id (model)', async function () {
+    sinon.stub(connection, 'execute').resolves([[allProducts[0]]]);
+    const result = await productsModels.getProductById(1);
+    expect(result).to.be.deep.equal(allProducts[0]);
+  });
+
+  afterEach(function () {
+    sinon.restore();
   });
 });
