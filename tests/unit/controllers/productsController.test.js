5eacfc834aab1a1a2546ac7422ba29d61d04d2ff
Leila Moraes <leila.violino@gmail.com>
2023-02-22 21:59:21 -0300

    test: adiciona teste para a função getProduct no controller

--- tests/unit/controllers/productsController.test.js
+++ tests/unit/controllers/productsController.test.js
@@ -4,13 +4,13 @@ const sinon = require("sinon");
 const sinonChai = require("sinon-chai");
 const { productsService } = require("../../../src/services");
 const { productsController } = require("../../../src/controllers");
-const { allProductsService } = require("../services/mock");
+const { allProductsService, productService } = require("../services/mock");
 const { allProducts } = require("../models/mock");
 
 chai.use(sinonChai);
 
-describe("Testa a camada controller para a rota /products", function () {
-  it("Retorna todos os produtos cadastrados (controller)", async function () {
+describe('Testa a camada controller para a rota /products', function () {
+  it('Retorna todos os produtos cadastrados (controller)', async function () {
     const req = {};
     const res = {};
 
@@ -23,5 +23,24 @@ describe("Testa a camada controller para a rota /products", function () {
 
     expect(res.status).to.have.been.calledWith(200);
     expect(res.json).to.have.been.calledWith(allProducts);
+  });
+
+  it('Retorna um produto pelo id (controller)', async function () {
+    const req = { params: { id: 1 } };
+    const res = {};
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon.stub(productsService, 'getProductById').resolves(productService);
+
+    await productsController.getProduct(req, res);
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(allProducts[0]);
+  });
+
+  afterEach(function () {
+    sinon.restore();
   });
 });
