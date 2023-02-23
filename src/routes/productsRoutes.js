3480000c133610d38b224458226ce3817f9b7665
Leila Moraes <leila.violino@gmail.com>
2023-02-23 16:20:37 -0300

    feat: acrescenta validação do nome na rota post

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -3,12 +3,13 @@ const express = require('express');
 const productsRouter = express.Router();
 
 const { productsController } = require('../controllers');
+const nameValidation = require('../middlewares/nameValidation');
 
 productsRouter.get('/', productsController.getProducts);
 
 productsRouter.get('/:id', productsController.getProduct);
 
-productsRouter.post('/', productsController.insertProduct);
+productsRouter.post('/', nameValidation, productsController.insertProduct);
 
 productsRouter.delete('/:id', productsController.deleteProduct);
 
