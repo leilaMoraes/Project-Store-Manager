37edf49d25bbb4a158fccba7cdb94397e2dbe906
Leila Moraes <leila.violino@gmail.com>
2023-02-22 21:28:32 -0300

    fix: troca as arrow functions por function nos testes

--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -1,11 +1,11 @@
 const { expect } = require("chai");
 const sinon = require("sinon");
 const connection = require("../../../src/models/connection");
-const { productsModels } = require("../../../src/models/index");
+const { productsModels } = require("../../../src/models");
 const { allProducts } = require("./mock");
 
-describe("Testa a camada model para a rota /products", () => {
-  it("Retorna todos os produtos cadastrados (model)", async () => {
+describe("Testa a camada model para a rota /products", function () {
+  it("Retorna todos os produtos cadastrados (model)", async function () {
     sinon.stub(connection, "execute").resolves([allProducts]);
     const result = await productsModels.getAllProducts();
     expect(result).to.be.deep.equal(allProducts);
--- tests/unit/services/productsService.test.js
+++ tests/unit/services/productsService.test.js
@@ -1,12 +1,12 @@
 const { expect } = require("chai");
 const sinon = require("sinon");
-const { productsModels } = require("../../../src/models/index");
-const { productsService } = require("../../../src/services/index");
+const { productsModels } = require("../../../src/models");
+const { productsService } = require("../../../src/services");
 const { allProducts } = require("../models/mock");
 const { allProductsService } = require("./mock");
 
-describe("Testa a camada service para a rota /products", () => {
-  it('Retorna todos os produtos cadastrados (service)', async () => {
+describe("Testa a camada service para a rota /products", function () {
+  it('Retorna todos os produtos cadastrados (service)', async function () {
     sinon.stub(productsModels, 'getAllProducts').resolves(allProducts);
     const result = await productsService.getAllProducts();
     expect(result).to.be.deep.equal(allProductsService);
