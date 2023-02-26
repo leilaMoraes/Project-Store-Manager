7222ca9f460d69b6aa1238d1d61eb246c10a119a
Leila Moraes <leila.violino@gmail.com>
2023-02-26 09:23:53 -0300

    feat: função model para exibir uma venda

--- src/models/salesModel.js
+++ src/models/salesModel.js
@@ -11,4 +11,16 @@ const getAllSales = async () => {
   return result;
 };
 
-module.exports = { getAllSales };
+const getSaleById = async (id) => {
+  const [[result]] = await connection.execute(
+    `SELECT s.date, sp.product_id AS productId, sp.quantity
+    FROM StoreManager.sales s
+    JOIN StoreManager.sales_products sp
+    ON s.id = sp.sale_id
+    WHERE s.id = ?
+    ORDER BY productId`, [id],
+  );
+  return result;
+};
+
+module.exports = { getAllSales, getSaleById };
