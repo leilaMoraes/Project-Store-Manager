f7b239b8cce5f7e306443231ec6452d0cff9bfa5
Leila Moraes <leila.violino@gmail.com>
2023-02-26 09:46:35 -0300

    feat: acrescenta rota /sales no app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { productsRoutes } = require('./routes');
+const { productsRoutes, salesRoutes } = require('./routes');
 
 const app = express();
 app.use(express.json());
@@ -10,6 +10,7 @@ app.get('/', (_request, response) => {
 });
 
 app.use('/products', productsRoutes);
+app.use('/sales', salesRoutes);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
