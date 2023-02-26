bfee3f06a4be87238b08333623b7f472c7018b4c
Leila Moraes <leila.violino@gmail.com>
2023-02-26 09:33:04 -0300

    feat: função service para exibir uma venda

--- src/services/salesService.js
+++ src/services/salesService.js
@@ -1,10 +1,17 @@
 const { salesModel } = require('../models');
 
 const OK = 200;
+const NOT_FOUND = 404;
 
 const getAllSales = async () => {
   const allSales = await salesModel.getAllSales();
   return { type: OK, message: allSales };
 };
 
-module.exports = { getAllSales };
+const getSaleById = async (id) => {
+  const sale = await salesModel.getSaleById(id);
+  if (sale) return { type: OK, message: sale };
+  return { type: NOT_FOUND, message: 'Sale not found' };
+};
+
+module.exports = { getAllSales, getSaleById };
