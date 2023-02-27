2830138bf75446898742244e267dcb59c3006a11
Leila Moraes <leila.violino@gmail.com>
2023-02-26 22:51:48 -0300

    test: teste da função getSale da camada controller

--- tests/unit/controllers/salesController.test.js
+++ tests/unit/controllers/salesController.test.js
@@ -4,8 +4,8 @@ const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 const { salesService } = require('../../../src/services');
 const { salesController } = require('../../../src/controllers');
-const { allSalesService } = require('../services/mock');
-const { allSales } = require('../models/mock');
+const { allSalesService, saleService, notFoundSale } = require('../services/mock');
+const { allSales, saleById } = require('../models/mock');
 
 chai.use(sinonChai);
 
@@ -27,5 +27,36 @@ describe('Testa a camada controller para a rota /sales', function () {
 
     expect(res.status).to.have.been.calledWith(200);
     expect(res.json).to.have.been.calledWith(allSales);
+  });
+
+
+  it('2) Retorna vendas com mesmo id (controller)', async function () {
+    const req = { params: { id: 1 } };
+    const res = {};
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon.stub(salesService, 'getSaleById').resolves(saleService);
+
+    await salesController.getSale(req, res);
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(saleById);
+  });
+
+  it('3) Retorna erro ao tentar retornar vendas com mesmo id (controller)', async function () {
+    const req = { params: { id: 0 } };
+    const res = {};
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon.stub(salesService, 'getSaleById').resolves(notFoundSale);
+
+    await salesController.getSale(req, res);
+
+    expect(res.status).to.have.been.calledWith(404);
+    expect(res.json).to.have.been.calledWith({ message: 'Sale not found' });
   });
 });
