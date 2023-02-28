ae0e3bd3c7f2d238d1c25756bbf8c9381abc8171
Leila Moraes <leila.violino@gmail.com>
2023-02-28 20:14:22 -0300

    feat: função service para deletar uma venda pelo id

--- src/services/salesService.js
+++ src/services/salesService.js
@@ -3,6 +3,7 @@ const { salesModel } = require('../models');
 const OK = 200;
 const NOT_FOUND = 404;
 const MESSAGE = { message: 'Sale not found' };
+const DELETED = 204;
 
 const getAllSales = async () => {
   const allSales = await salesModel.getAllSales();
@@ -15,4 +16,11 @@ const getSaleById = async (id) => {
   return { type: NOT_FOUND, message: MESSAGE };
 };
 
-module.exports = { getAllSales, getSaleById };
+const deleteSale = async (id) => {
+  const sale = await salesModel.getSaleById(id);
+  if (!sale.length) return { type: NOT_FOUND, message: MESSAGE };
+  await salesModel.deleteSale(id);
+  return { type: DELETED, message: '' };
+};
+
+module.exports = { getAllSales, getSaleById, deleteSale };
